37588b78537b6cad2b567cce4f2e3933315f6755
brekc <brekchiles@gmail.com>
2026-09-10 20:46:56 -0700

    Corrected domain for Light Gray Canvas basemap

--- geotagged-photo-mapper-demo/geotagged-photo-mapper.js
+++ geotagged-photo-mapper-demo/geotagged-photo-mapper.js
@@ -6,7 +6,7 @@ const basemaps = {
     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 19,
   }),
-  'Light Gray (Esri)': L.tileLayer('https://server.arcgis.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+  'Light Gray (Esri)': L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Esri, HERE, Garmin, © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 16,
   }),
